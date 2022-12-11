fc5aa40a16f60d894c8a30746a156739130b9fb6
jyotir <jyotir@pop-os.localdomain>
2022-12-11 11:37:06 -0500

    created routes and conrollers files

---
+++ routes/AppRouter.js
@@ -0,0 +1,4 @@
+const Router = require('express').Router()
+const MovieRouter = require('./MovieRouter')
+const UserRouter = require('./UserRouter')
+const WatchlistRouter = require('./WatchlistRouter')
