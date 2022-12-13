35fe50f0ce8bfb05ad0ed7ef8bed2ea3b25a79c3
jyotir <jyotir@pop-os.localdomain>
2022-12-12 20:39:34 -0500

    created registration functionality

---
+++ controllers/AuthController.js
@@ -0,0 +1,27 @@
+const { User } = require('../sequelize/models')
+const middleware = require('../middleware')
+
+const Login = async (req, res) => {
+    try {
+    
+    } catch (error) {
+        throw error
+    }
+}
+
+const Register = async (req, res) => {
+    try{
+        const { email, password, name } = req.beforeDestroy()
+        let passwordDigest = await middleware.hashPassword(password)
+        const user = await User.create({ email, passwordDigest, name })
+        res.send(user)
+    } catch (error) {
+        throw error
+    }
+}
+
+
+module.exports = {
+    Login,
+    Register
+}
